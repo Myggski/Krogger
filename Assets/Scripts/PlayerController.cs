d880fd05f4085da255648b27fb413ce1c5a5a736
Tomas Wallin <Myggski@users.noreply.github.com>
2021-08-25 20:55:03 +0200

    Syncing rotation, animation and movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 namespace FG {
 	[RequireComponent(typeof(Rigidbody))]
@@ -15,17 +27,25 @@ namespace FG {
 		[SerializeField]
 		private Animator _animator;
 
-		[SerializeField] private FloatEvent _jumpEvent;
+		private Coroutine _speedUpCoroutine;
 
+		private float _horizontalMovement = 0f;
+		private float _verticalMovement = 0f;
 		private bool _jumping = false;
+		private bool _isKeyPressedDown = false;
+		private bool _isCurrentlyRunning = false;
+		private bool _inputStarted = false;
 		private Vector3 _nextDirection;
 		private float _rotationAngle = 0f;
 
-		private List<string> _keysPressed;
 		private List<Vector3> _queuedMovements;
 		private Vector3 _currentPosition;
 		private Rigidbody _body;
-		
+
+		private bool IsMovingHorizontal => !_horizontalMovement.Equals(0f);
+		private bool IsMovingVertical => !_verticalMovement.Equals(0f);
+		private bool IsMoving => IsMovingHorizontal || IsMovingVertical;
+		private bool HasNoKeysPressedDown => !Keyboard.current.anyKey.isPressed;
 		private Vector3 NextDirection => _queuedMovements.Any() ? _queuedMovements[0] : Vector3.zero;
 
 		/// <summary>
@@ -33,24 +53,34 @@ namespace FG {
 		/// </summary>
 		/// <param name="value"></param>
 		public void OnMovement(InputAction.CallbackContext value) {
-			if (value.phase == InputActionPhase.Started) {
-				QueueNewDirection(value.control.displayName);
+			Vector2 inputMovement = value.ReadValue<Vector2>();
+
+			_horizontalMovement = inputMovement.x;
+			_verticalMovement = inputMovement.y;
+
+			if (value.started) {
+				QueueNewDirection();
 			}
 		}
 
-		private void QueueNewDirection(string keyPressed) {
-			_nextDirection = InputHelper.GetMovingDirectionByKey(keyPressed);
+		private void QueueNewDirection() {
+			_nextDirection = Vector3.zero;
+
+			if (IsMovingHorizontal) {
+				_nextDirection.x = _horizontalMovement;
+			} else if (IsMovingVertical) {
+				_nextDirection.z = _verticalMovement;
+			}
 
 			if (_queuedMovements.Any() && _queuedMovements.Count < 3) {
-				_queuedMovements.Add(_queuedMovements[_queuedMovements.Count - 1] + _nextDirection * _movementStepsInUnits);
+				_queuedMovements.Add(_queuedMovements[_queuedMovements.Count - 1] + (_nextDirection * _movementStepsInUnits));
 			}
 			else {
-				_queuedMovements.Add( new Vector3(_currentPosition.x, transform.position.y, _currentPosition.z) +
-				                      (_nextDirection * _movementStepsInUnits));
-				
-				_rotationAngle = Quaternion.Angle(transform.rotation,
-					Quaternion.LookRotation((NextDirection - transform.position).normalized));
+				_queuedMovements.Add(new Vector3(_currentPosition.x, transform.position.y, _currentPosition.z) +
+				                     (_nextDirection * _movementStepsInUnits));
 			}
+			
+			_rotationAngle = IsMoving ? Quaternion.Angle(transform.rotation, Quaternion.LookRotation((NextDirection - transform.position).normalized)) : 0;
 		}
 
 		/// <summary>
@@ -59,10 +89,16 @@ namespace FG {
 		private void ResetMovementInfo() {
 			_jumping = false;
 			_currentPosition = transform.position;
+		}
 
-			if (_queuedMovements.Count > 0) {
-				_rotationAngle = Quaternion.Angle(transform.rotation,
-					Quaternion.LookRotation((NextDirection - transform.position).normalized));
+		/// <summary>
+		/// Sets the moving direction and rotation angle
+		/// </summary>
+		private void SetupMovementInfo() {
+			ResetMovementInfo();
+
+			if (IsMoving) {
+				_isKeyPressedDown = true;
 			}
 		}
 		
@@ -75,9 +111,7 @@ namespace FG {
 			}
 
 			_jumping = true;
-			_jumpEvent.Invoke(_movementSpeed / _movementStepsInUnits);
-
-			_body.AddForce(0, _jumpForce, 0, ForceMode.Force);
+			_body.AddForce(0, this._jumpForce, 0, ForceMode.Force);
 			_animator.SetTrigger("Jump");
 		}
 
@@ -93,7 +127,7 @@ namespace FG {
 
 			if (transform.position == NextDirection) {
 				_queuedMovements.RemoveAt(0);
-				ResetMovementInfo();
+				SetupMovementInfo();
 			}
 		}
 
@@ -102,21 +136,23 @@ namespace FG {
 		/// </summary>
 		private void Setup() {
 			_body = GetComponent<Rigidbody>();
-			_currentPosition = transform.position;
+			_currentPosition = this.transform.position;
 			_animator.speed = _movementSpeed / _movementStepsInUnits;
 			_queuedMovements = new List<Vector3>();
-			_keysPressed = new List<string>();
 		}
 
 		/// <summary>
 		/// Check if there are movement, and move if it's true
 		/// </summary>
 		private void CheckForMovement() {
-			if (_queuedMovements.Count > 0) {
+			if (!_isKeyPressedDown) {
+				_isCurrentlyRunning = false;
+				SetupMovementInfo();
+			} else if (_queuedMovements.Count > 0) {
 				MoveAndRotate();
-			}
-			else {
-				ResetMovementInfo();
+				_isCurrentlyRunning = true;
+			} else if (HasNoKeysPressedDown) {
+				_isKeyPressedDown = false;
 			}
 		}
 		
