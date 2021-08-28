ee86eada1c7a98f244675a392045b9b463b1d2c7
Tomas Wallin <Myggski@users.noreply.github.com>
2021-08-28 17:12:44 +0200

    Added Start / Pause menu

---
+++ Assets/Scripts/Attributes/Scene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace FG {
+	/// <summary>
+	/// Attribute to set above a string property to be able to select a scene in the inspector
+	/// </summary>
+	public class SceneAttribute : PropertyAttribute {
+	}
+}
